9432f47ee0d091da9ab3584c94192a91c4fe7d49
SudoCode76 <bo3321319@gmail.com>
2024-01-06 18:13:13 -0400

    menu principal

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Estadistica_Descriptiva_Calculadora;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        menuPrincipal.MostrarMenu();
+        while (true)
+        {
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine("Calcular rango");
+                    break;
+                case "2":
+                    Console.WriteLine("Calcular numero de intervalos");
+                    break;
+                case "3":
+                    Console.WriteLine("Calcular Amplitud");
+                    break;
+                case "4":
+                    Console.WriteLine("Ordernar numeros de mayor a menor");
+                    break;
+                case "5":
+                    Console.WriteLine("Mostrar marca de clase");
+                    break;
+                case "6":
+                    Console.WriteLine("Saliendo del programa...");
+                    return;
+                default:
+                    Console.WriteLine("Opción no válida. Por favor, selecciona una opción del menú.");
+                    break;
+            }
+
+            menuPrincipal.MostrarMenu();
+        }
+    }
+}
---
+++ menuPrincipal.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estadistica_Descriptiva_Calculadora
+{
+    public class menuPrincipal
+    {
+        public static void MostrarMenu()
+    {
+        Console.WriteLine("----- Calculadora Est. Desriptiva By Sudo -----");
+        Console.WriteLine("1. Calcular rango");
+        Console.WriteLine("2. Calcular numero de intervalos");
+        Console.WriteLine("3. Calcular Amplitud");
+        Console.WriteLine("4. Ordernar numeros de mayor a menor");
+        Console.WriteLine("5. Mostrar marca de clase");
+        Console.WriteLine("6. Salir");
+        Console.Write("Selecciona una opción: ");
+    }
+        
+    }
+}
